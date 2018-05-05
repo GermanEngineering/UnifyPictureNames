d746f36fd8c6ec4e9ade3e6064a0bc873842b3fb
GermanEngineering <peter@trustmeimanengineer.de>
2018-05-05 13:08:25 +0200

    multiple improvements

--- Program.py
+++ Program.py
@@ -2,21 +2,30 @@ import exifread
 from shutil import copy
 import os
 
-for file in os.listdir("input"):
-    if file.lower().endswith(".jpg"):
-        image = open("input\\{0}".format(file), "rb")
-        metadata = exifread.process_file(image)
-        try:
-            device = str(metadata["Image Make"])
-        except Exception:
-            print("No device name found for: \"{0}\"".format(file))
-            device = ""
+for path, subDirectories, files in os.walk("input"):
+    for file in files:
 
+        # create output path
+        if not os.path.exists(path.replace("input", "output")):
+            os.makedirs(path.replace("input", "output"))
+
+        # get file metadata
+        metadata = exifread.process_file(open(os.path.join(path, file), "rb"))
+        captureTime = ""
+        try:
+            captureTime = str(metadata["EXIF DateTimeOriginal"])
+        except Exception:
+            print("No capture time found for: \"{0}\"".format(os.path.join(path, file)))
+
+        # WhatsApp images
         if "WA" in file:
             newFileName = file.replace("IMG-", "")
-            copy("input\\{0}".format(file), "output\\{0}".format(file))
-        elif device == "Apple":
-            newFileName = str(metadata["EXIF DateTimeOriginal"]).replace(":", "").replace(" ", "_") + ".jpg"
-            copy("input\\{0}".format(file), "output\\{0}".format(newFileName))
+            copy(os.path.join(path, file), os.path.join(path.replace("input", "output"), newFileName))
+        # pictures with creation timestamp
+        elif captureTime != "":
+            _, fileExtension = os.path.splitext(file)
+            newFileName = captureTime.replace(":", "").replace(" ", "_") + fileExtension.lower()
+            copy(os.path.join(path, file), os.path.join(path.replace("input", "output"), newFileName))
+        # files without creation timestamp
         else:
-            copy("input\\{0}".format(file), "output\\{0}".format(file))
+            copy(os.path.join(path, file), os.path.join(path.replace("input", "output"), file))
